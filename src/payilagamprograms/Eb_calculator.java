6ca374846a8b458150038a7149e8566eb949232a
venkatesh <venkyvenky883@gmail.com>
2023-07-03 15:00:43 +0530

    Eb calculator

---
+++ src/payilagamprograms/Eb_calculator.java
@@ -0,0 +1,11 @@
+package payilagamprograms;
+import java.util.*;
+public class Eb_calculator {
+
+	public static void main(String[] args)
+	{
+		Scanner io=new Scanner(System.in);
+		int n=io.nextInt();
+		
+	}
+}
